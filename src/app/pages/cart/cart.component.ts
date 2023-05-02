4f4afa681e30bc3c7f1efc83edcfda7c9ff83217
Marcelearn <jaworskimarcel@gmail.com>
2023-05-02 10:50:23 +0200

    Cart table

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -6,10 +6,23 @@ import { Cart, CartItem } from 'src/app/models/cart.model';
   templateUrl: './cart.component.html'
 })
 export class CartComponent {
-  cart: Cart = { items: []};
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'Snikers',
+    price: 150,
+    quantity: 1,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'Snikers',
+    price: 150,
+    quantity: 3,
+    id: 2,
+  }]};
 
   dataSource: Array<CartItem> = [];
-  dispatedColumns: Array<string> = [
+  displayedColumns: Array<string> = [
     'product',
     'name',
     'price',
@@ -20,5 +33,11 @@ export class CartComponent {
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.
+      map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
 }
