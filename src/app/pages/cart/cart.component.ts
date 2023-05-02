6f18d4f4b39a4eaf97b81fa4f9b3397d8ef7fe0e
Marcelearn <jaworskimarcel@gmail.com>
2023-05-02 10:03:55 +0200

    cart component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './cart.component.html'
 })
 export class CartComponent {
+  cart: Cart = { items: []};
 
+  dataSource: Array<CartItem> = [];
+  dispatedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ];
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
 }
