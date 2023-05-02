83c7d1db42106e2a7f2b2b96aef4dd500f01930e
Marcelearn <jaworskimarcel@gmail.com>
2023-05-02 11:54:19 +0200

    cart service

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
 
 @Component({
   selector: 'app-product-box',
@@ -6,4 +8,17 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: 'Snikers',
+    price: 150,
+    category: 'shoes',
+    description: 'Description',
+    image: 'https://via.placeholder.com/150'
+  };
+  @Output() addToCart = new EventEmitter();
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
