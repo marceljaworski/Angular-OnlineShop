105c41833dc4a0141101cd6e6213c79d9ce73e9c
Marcelearn <jaworskimarcel@gmail.com>
2023-05-03 10:42:58 +0200

    cart.subscribe

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -35,10 +35,17 @@ export class CartComponent {
   constructor(private cartService: CartService) {  }
 
   ngOnInit(): void {
-    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
   }
 
   getTotal(items: Array<CartItem>): number {
     return this.cartService.getTotal(items);
+  }
+
+  onClearCart():void {
+    this.cartService.clearCart();
   }
 }
