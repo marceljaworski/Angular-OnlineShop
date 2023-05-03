f00ab9f99d8a06158537fd74f298bdadc1be8e82
Marcelearn <jaworskimarcel@gmail.com>
2023-05-03 12:43:41 +0200

    dinamic products

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
 
 const ROWS_HEIGHT: { [id:number]: number} = { 1: 400, 3: 335, 4: 350 };
 
@@ -8,16 +10,33 @@ const ROWS_HEIGHT: { [id:number]: number} = { 1: 400, 3: 335, 4: 350 };
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   cols = 3;
   rowHeight = ROWS_HEIGHT [this.cols];
   category: string | undefined;
+  products: Array<Product> | undefined;
+  sort = 'desc';
+  count = '12';
+  productsSubscription: Subscription | undefined;
 
-  constructor(private cartService: CartService) {}
+
+  constructor(private cartService: CartService, private storeService: StoreService) {}
+  
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT [this.cols];
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productsSubscription = this.storeService.getAllProducts(this.count, this.sort)
+      .subscribe((_products) => {
+        this.products = _products;
+      })
   }
 
   onShowCategory(newCategory: string): void {
@@ -32,5 +51,10 @@ export class HomeComponent {
       quantity: 1,
       id: product.id
     })
+  }
+  ngOnDestroy(): void {
+    if (this.productsSubscription) {
+      this.productsSubscription.unsubscribe();
+    }
   }
 }
