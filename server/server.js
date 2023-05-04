54c1abe5c1f41c64058f85710cc469117c48b709
Marcelearn <jaworskimarcel@gmail.com>
2023-05-04 11:42:54 +0200

    checkout endpoint

--- server/server.js
+++ server/server.js
@@ -13,3 +13,25 @@ app.use(cors({ origin: true, credentials: true}));
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
 
 const stripe = require("stripe")(`${STRIPE_SECRET_KEY}`)
+
+app.post("/checkout", async (req, res, next) => {
+    try{
+        const session = await stripe.checkout.sessions.create({
+            line_items: req.body.items.map((item) => ({
+                currency: "EUR",
+                Product_data: {
+                    name: item.name,
+                    images: [item.product]
+                },
+                unit_amount: item.price * 100
+            })),
+            mode: "payment",
+            succes_url: "http://localhost:4242/success.html",
+            cancel_url: "http://localhost:4242/cancel.html",
+        });
+
+        res.status(200).json(session);
+    } catch (error) {
+        next(error);
+    }
+});
