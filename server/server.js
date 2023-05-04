a027a6a35f8d2bc38374f64530dcadbe26709f97
Marcelearn <jaworskimarcel@gmail.com>
2023-05-04 11:24:21 +0200

    dotenv secret stripe_key

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config()
 const express = require("express");
 const cors = require("cors");
 const bodyparser = require("body-parser");
@@ -7,3 +9,7 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: flase }));
 app.use(bodyparser.json());
 app.use(cors({ origin: true, credentials: true}));
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
+
+const stripe = require("stripe")(`${STRIPE_SECRET_KEY}`)
