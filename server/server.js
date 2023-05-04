b4244c87b94322ca75ef00c1d6bd10250aaf6b66
Marcelearn <jaworskimarcel@gmail.com>
2023-05-04 12:59:27 +0200

    stripe shipping added

--- server/server.js
+++ server/server.js
@@ -19,6 +19,51 @@ app.post("/checkout", async (req, res, next) => {
     console.log(req.body)
     try{
         const session = await stripe.checkout.sessions.create({
+            shipping_address_collection: {
+                allowed_countries: ['DE', 'PL', 'FR', 'AT'],
+              },
+                shipping_options: [
+                  {
+                    shipping_rate_data: {
+                      type: 'fixed_amount',
+                      fixed_amount: {
+                        amount: 0,
+                        currency: 'EUR',
+                      },
+                      display_name: 'Free shipping',
+                      delivery_estimate: {
+                        minimum: {
+                          unit: 'business_day',
+                          value: 5,
+                        },
+                        maximum: {
+                          unit: 'business_day',
+                          value: 7,
+                        },
+                      },
+                    },
+                  },
+                  {
+                    shipping_rate_data: {
+                      type: 'fixed_amount',
+                      fixed_amount: {
+                        amount: 1500,
+                        currency: 'EUR',
+                      },
+                      display_name: 'Next day air',
+                      delivery_estimate: {
+                        minimum: {
+                          unit: 'business_day',
+                          value: 1,
+                        },
+                        maximum: {
+                          unit: 'business_day',
+                          value: 1,
+                        },
+                      },
+                    },
+                  },
+                ],
             line_items: req.body.items.map((item) => ({
                 price_data: {
                     currency: 'EUR',
